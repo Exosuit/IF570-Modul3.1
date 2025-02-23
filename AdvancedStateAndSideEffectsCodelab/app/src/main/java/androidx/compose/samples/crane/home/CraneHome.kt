d16d7497d8260802e0fda1a0d60cd93ad27bdc77
Opengheimer <intltcsfleader@gmail.com>
2025-02-23 19:04:01 +0700

    consuming a flow safely from the viewmodel

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
@@ -36,6 +36,7 @@ import androidx.compose.samples.crane.base.ExploreSection
 import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 typealias OnExploreItemClicked = (ExploreModel) -> Unit
@@ -76,7 +77,7 @@ fun CraneHomeContent(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = viewModel(),
 ) {
-    // TODO Codelab: collectAsStateWithLifecycle step - consume stream of data from the ViewModel
+    val suggestedDestinations by viewModel.suggestedDestinations.collectAsStateWithLifecycle()
     val suggestedDestinations: List<ExploreModel> = remember { emptyList() }
 
     val onPeopleChanged: (Int) -> Unit = { viewModel.updatePeople(it) }
